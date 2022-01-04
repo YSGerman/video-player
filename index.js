c61f354d62522b8f17c675bc163c90e4217da820
YSGerman <60149851+YSGerman@users.noreply.github.com>
2022-01-04 05:10:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const $video = document.querySelector('#video')
+const $play = document.querySelector('#play')
+const $pause = document.querySelector('#pause')
+const $backw = document.querySelector('#backw')
+const $fordw = document.querySelector('#fordw')
+
+$play.addEventListener('click', handlePlay)
+$pause.addEventListener('click', handlePause)
+$backw.addEventListener('click', handleBackw)
+$fordw.addEventListener('click', handleFordw)
+
+function handlePlay() {
+  $video.play()
+  $play.hidden = true
+  $pause.hidden = false
+  console.log('le diste click a play')
+}
+
+function handlePause() {
+  $video.pause()
+  $play.hidden = false
+  $pause.hidden = true
+  console.log('le diste click a pause')
+}
+
+function handleBackw() {
+  $video.currentTime -= 5
+  console.log('le diste click a -5 seg', $video.currentTime)
+}
+
+function handleFordw() {
+  $video.currentTime += 5
+  console.log('le diste click a +5 seg', $video.currentTime)
+}
+
+const $progress = document.querySelector('#progress')
+
+$video.addEventListener('loadedmetadata', handleMetadata)
+$video.addEventListener('timeupdate', handleTimeUpdate)
+
+function handleMetadata() {
+  $progress.max = $video.duration
+  console.log('El video ya cargo', $video.duration)
+}
+
+function handleTimeUpdate() {
+  $progress.value = $video.currentTime
+  console.log('tiempo actual', $video.currentTime)
+}
+
+$progress.addEventListener('input', handleInput)
+
+function handleInput() {
+  $video.currentTime = $progress.value
+  console.log($progress.value)
+}
